55226ab0565487a091245c630a42b9e74448fec9
V.Shreashta <shravanishreashta@gmail.com>
2026-09-03 17:54:34 +0530

    Add files via upload

---
+++ ifelseladder.java
@@ -0,0 +1,10 @@
+public class ifelseladder {
+public static void main(String[] args) {
+ int income =100000;
+ if(income>100000)
+ {System.out.println("he is rich");}
+ else if (income <100000 && income>=50000)
+ {System.out.println("he is middle man");}
+ else
+ {System.out.println("he is poor");}
+}}
