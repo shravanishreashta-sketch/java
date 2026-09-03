d1747c19bce2f15016148b95f4e3543ac4b101d3
V.Shreashta <shravanishreashta@gmail.com>
2026-09-03 17:55:03 +0530

    Add files via upload

---
+++ simpleif.java
@@ -0,0 +1,9 @@
+public class simpleif {
+public static void main(String[] args) {
+ int a=10;
+    if (a>=20)
+        {System.out.println("a is greater  than 20");}
+ {System.out.println("a is less than 20 ");}
+
+}
+}
