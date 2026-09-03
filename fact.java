7bafb69bc886bba4a34bfd31ee353305093ce52f
V.Shreashta <shravanishreashta@gmail.com>
2026-09-03 17:50:20 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,9 @@
+public cl`ss factorial {
+    public static void main (String[] args) {
+        int num =10;
+        long factorial =1;
+        for(int i=1;i<=num;i++){
+            factorial*=i;
+        }
+          system.out.println("Factorial of "+ num +" is:"+factorial);}}
+}
