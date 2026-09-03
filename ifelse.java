4fe1c189790b74d390865eae5607cef4d8225af8
V.Shreashta <shravanishreashta@gmail.com>
2026-09-03 17:53:24 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,8 @@
+public class ifelse {
+public static void main(String[] args) {
+ int age=40;
+ if(age>=18)
+  {System.out.println("he is eligible to vote");}
+ else
+ {System.out.println("he is not eligible to vote");}
+}}
