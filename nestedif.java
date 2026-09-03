cdcc9b41a7731513e0805526cbfdc653299f6f32
V.Shreashta <shravanishreashta@gmail.com>
2026-09-03 17:55:38 +0530

    Add files via upload

---
+++ nestedif.java
@@ -0,0 +1,14 @@
+public class nestedif {
+public static void main(String[] args) {
+ int x=221;
+ if(x%13==0)
+ {System.out.println("x is divisible by 13");
+    if(x%17==0)
+    {System.out.println("x is divisible by 17");}
+    else
+    {System.out.println("x is not divisible by 17");}}
+ else
+ {System.out.println("x is not divisible by 13");}
+ }
+
+}
