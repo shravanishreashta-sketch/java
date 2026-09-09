ec063f061fcaee0f4953b516480435bc6e536a40
V.Shreashta <shravanishreashta@gmail.com>
2026-09-09 11:45:26 +0530

    Add files via upload

---
+++ Mulitiplication.java
@@ -0,0 +1,12 @@
+public class Mulitiplication {
+    public static void main(String[] args) {
+        int num = 8; // The number for the multiplication table
+
+        // Loop from 1 to 10 to print the table
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
+   
+
