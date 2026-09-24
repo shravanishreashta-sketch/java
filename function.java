4839a46dedafb1643b1f8925ef423f745a981d4c
V.Shreashta <shravanishreashta@gmail.com>
2026-09-24 10:50:38 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,8 @@
+public static int Square(int num){
+    return num * num;
+}
+public static void main(String[] args) {
+    int result;
+    result = Square(5);
+    System.out.println("The square of 5 is: " + result);
+}
